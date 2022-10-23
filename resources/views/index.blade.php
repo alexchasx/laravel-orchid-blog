fc62dc7bf747067a8011278fcd2bc89ce5792b4b
Alexander Chasovnikov <alexchasx@gmail.com>
2022-10-23 16:59:17 +0300

    add text to contact page; refactoring index view

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -6,39 +6,42 @@
 
 @if (!empty($articles[0]))
 
-@foreach ($articles as $article)
+    @foreach ($articles as $article)
 
-<article class="article_card article_card_index">
-    <h3 class="article_title"><a href="{{route('articleShow', $article->id)}}" class="continue_read">
-            {{ $article->title }}
-        </a>
-    </h3>
+        <article class="article_card article_card_index">
+            <h3 class="article_title"><a href="{{route('articleShow', $article->id)}}" class="continue_read">
+                    {{ $article->title }}
+                </a>
+            </h3>
 
-    @include('includes/publication_date')
+            @include('includes/publication_date')
 
-    <!-- <p>{{ Str::limit($article->excert, 200) }}</p> -->
+            <!-- <p>{{ Str::limit($article->excert, 200) }}</p> -->
 
-    <!-- <a href="{{route('articleShow', $article->id)}}" class="continue_read">
-    {{ __('Читать далее') }}
-    </a> -->
-</article>
+            <!-- <a href="{{route('articleShow', $article->id)}}" class="continue_read">
+            {{ __('Читать далее') }}
+            </a> -->
+        </article>
 
-@php
-$article = null; // влияет на работу мета-тегов
-@endphp
+        @php
+            $article = null; // влияет на работу мета-тегов
+        @endphp
 
-@endforeach
+    @endforeach
+
+    <nav>
+        <ul class="pagination">
+            {{ $articles->links('vendor.pagination.default') }}
+        </ul>
+    </nav>
 
 @else
-<div class="article_card">
-    {{ __('Ничего не нашлось') }}
-</div>
+
+    <div class="article_card">
+        {{ __('Ничего не нашлось') }}
+    </div>
+
 @endif
 
-<nav>
-    <ul class="pagination">
-        {{ $articles->links('vendor.pagination.default') }}
-    </ul>
-</nav>
 
 @endsection
